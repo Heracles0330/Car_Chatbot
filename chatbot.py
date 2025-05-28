4f9ee018d8245a3e1d4e81b21d8c9ccd802939f7
Heracles0330 <heracles0330@gmail.com>
2025-05-28 09:14:59 +0000

    docs: emphasize RC vehicle search criteria in chatbot documentation

--- chatbot.py
+++ chatbot.py
@@ -109,7 +109,9 @@ Your Task:
 7.  If the sql search has failed for 2 times, try only using pinecone search.
 8.  The popularity criteria is the total_sold column in the products table.
 9.  if the third search is not successful, then answer that there is no product that matches the user's query.
-10. If the user asks for the RC vehicles, then search the products using the global_type as "car" and name must contain "RC".
+
+**Important**:
+If the user asks for the RC vehicles, then search the products using the global_type as "car" and name must contain "RC".
 
 **You also have access to a tool called "get_order".
 
